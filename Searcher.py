2bf81278e0e47a2567489a87df17764662a767f3
emartensson <75781290+emartensson@users.noreply.github.com>
2021-04-26 16:10:55 +0200

    Update Searcher.py

--- Searcher.py
+++ Searcher.py
@@ -1,19 +1,8 @@
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Document, Text, Boolean, Integer
+from elasticsearch_dsl import Search
 from elasticsearch_dsl.connections import connections
 
 connections.create_connection(hosts=['localhost'])
-class MyDoc(Document):
-    function_name = Text()
-    function_code = Text()
-    repo_name = Text()
-    repo_url = Text()
-    repo_stars = Integer()
-    isKth = Boolean()
-    kth_course_code = Text()
-
-    class Index:
-        name = 'dd2476_project'
 
 class Searcher:
 
@@ -23,22 +12,46 @@ class Searcher:
 
     def search(self, query, course_code=None):
         
-        q = query + " " + course_code if course_code != None else query
-        s = Search(using=self.client, index='dd2476_project').query("multi_match", query=q, fields=['function_name', 'kth_course_code'])
-        response = s.execute()
-      
-        return response.to_dict()['hits']['hits']
+        #q = query + " " + course_code if course_code != None else query
+        #s = Search(using=self.client, index='dd2476_project').query("regexp", query=q)#, fields=['function_name'])#, 'kth_course_code'])
+        #response = s.execute()
+        temp = {
+            "size":100,
+            "query": {
+                    "bool": {
+                        "should": [
 
-
-    def test(self):
-        s = Search(using=self.client,index='dd2476_project')
-        results = s.execute()
-        print(s.count())
-        print(results.hits.total)
-        for hit in s:
-            print(hit.to_dict())
+                        ]
+                    }
+        }}
+        if query:
+            temp["query"]["bool"]["should"].append(
+                {
+                    "multi_match": {
+                        "query": query,
+                        "fields": [
+                            "function_code",
+                            "function_name"
+                        ],
+                        "minimum_should_match": "80%"
+                    }
+                }
+            )
+        if course_code:
+            temp["query"]["bool"]["should"].append(
+                {
+                    "match": {
+                        "kth_course_code": {
+                            "query": course_code,
+                            "minimum_should_match": "20%"
+                        }
+                    }
+                }
+            )
+        test = self.client.search(index='dd2476_project_test', body=temp)
+        return test['hits']['hits']
+        
        
-
 if __name__=="__main__":
     s = Searcher()
 
